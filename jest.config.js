ee8c244a4dcba914b157cc6b956c210a92fe6806
Alan Ribeiro <alandson_ribeiro@hotmail.com>
2021-03-08 20:29:49 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    verbose: true,
+    clearMocks: true,
+    rootDir: ".",
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+
+
+};
---
+++ src/tests/FirstTest.spec.ts
@@ -0,0 +1,9 @@
+import { User } from '@models/User'
+
+test('it should be ok', () => {
+    const user = new User()
+
+    user.name = 'Alan'
+
+    expect(user.name).toEqual('Alan')
+})
