08cf8037fff9e0e942ba3bc25f2c1e160840ae97
Alan Ribeiro <alandson_ribeiro@hotmail.com>
2021-03-08 21:05:56 -0300

    Fix Jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-undef */
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
+
+const { compilerOptions } = require('./tsconfig.json')
+
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
 
 module.exports = {
     verbose: true,
     clearMocks: true,
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
     rootDir: ".",
     preset: 'ts-jest',
     testEnvironment: 'node',
